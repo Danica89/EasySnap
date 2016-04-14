91131b53acee4d27e231a2b0d200ccff07173c67
averatec <averatec1993@gmail.com>
2016-04-15 00:20:17 +0200

    Fixed and optimized tile draw tool.

--- src/main/java/org/easysnap/main/shell/capturetool/imagedrawtool/mouse/TileDrawTool.java
+++ src/main/java/org/easysnap/main/shell/capturetool/imagedrawtool/mouse/TileDrawTool.java
@@ -22,12 +22,20 @@ package org.easysnap.main.shell.capturetool.imagedrawtool.mouse;
 import org.eclipse.swt.events.MouseEvent;
 import org.eclipse.swt.graphics.*;
 
-/**
- * TODO: optymalizacja
- */
+import java.util.ArrayList;
+import java.util.Random;
+
 public class TileDrawTool extends AbstractMouseDrawTool {
+    private int offset;
+
     public TileDrawTool(Image image, Color color, int size) {
         super(image, color, size);
+    }
+
+    @Override
+    public synchronized Image onStart(MouseEvent event) {
+        offset = (offset < 1) ? 9 : --offset;
+        return super.onStart(event);
     }
 
     @Override
@@ -45,116 +53,58 @@ public class TileDrawTool extends AbstractMouseDrawTool {
             ImageData imageData = new ImageData(width, height, this.image.getImageData().depth, this.image.getImageData().palette);
             ImageData currentImageData = this.image.getImageData();
             int lineWidth = Math.max(2, graphicsContext.getLineWidth());
-            int boxSizeX = (int) Math.max(Math.ceil((double) width / (double) lineWidth), 1) + 1;
-            int boxSizeY = (int) Math.max(Math.ceil((double) height / (double) lineWidth), 1) + 1;
-            RGB[][] colors = new RGB[boxSizeX][boxSizeY];
-            int[][] size = new int[boxSizeX][boxSizeY];
-            PaletteData palette = currentImageData.palette;
-            int x;
-            int y;
             int startPosX = intersectRect.x;
             int endPosX = intersectRect.x + intersectRect.width;
             int startPosY = intersectRect.y;
             int endPosY = intersectRect.y + intersectRect.height;
+            PaletteData palette = currentImageData.palette;
 
-            //TODO: refactor
-            for (int i = startPosX; i <= endPosX; ++i) {
-                for (int j = startPosY; j <= endPosY; ++j) {
-                    try {
-                        if (i < 0 || j < 0) {
-                            continue;
-                        }
-                        int pixel = currentImageData.getPixel(i, j);
-                        RGB rgb = palette.getRGB(pixel);
-                        x = i - startPosX;
-                        y = j - startPosY;
-                        int inBoxX = (int) Math.round(((double) x) / ((double) lineWidth));
-                        int inBoxY = (int) Math.round(((double) y) / ((double) lineWidth));
-                        if (inBoxX < 0 || inBoxX > boxSizeX || inBoxY < 0 || inBoxY > boxSizeY) {
-                            continue;
-                        }
-                        if (colors[inBoxX][inBoxY] == null) {
-                            size[inBoxX][inBoxY] = 1;
-                            colors[inBoxX][inBoxY] = new RGB(rgb.red, rgb.green, rgb.blue);
-                            continue;
-                        }
-                        colors[inBoxX][inBoxY].red += rgb.red;
-                        colors[inBoxX][inBoxY].green += rgb.green;
-                        colors[inBoxX][inBoxY].blue += rgb.blue;
-                        size[inBoxX][inBoxY]++;
+            graphicsContext.setLineWidth(1);
+            ArrayList<Rectangle> grid = new ArrayList<Rectangle>();
 
-                    } catch (IllegalArgumentException ex) {
-                        //dupaaaaa
+            for (int x = (startPosX - startPosX % lineWidth); x < endPosX + offset; x+= lineWidth) {
+                for (int y = (startPosY - startPosY % lineWidth); y < endPosY + offset; y+=lineWidth) {
+                    if (x % lineWidth != 0 || y % lineWidth != 0) {
+                        continue;
+                    }
+                    Rectangle rect = new Rectangle(x - offset, y - offset, lineWidth, lineWidth);
+                    rect = intersectRect.intersection(rect);
+                    grid.add(rect);
+                }
+            }
+            int x, y;
+            RGB color;
+            for (Rectangle rect : grid) {
+                color = new RGB(0,0,0);
+                for (int i = 0; i < rect.width; i++) {
+                    x = rect.x + i;
+                    for (int j = 0; j < rect.height; j++) {
+                        y = rect.y + j;
+                        int pixel = currentImageData.getPixel(x,y);
+                        RGB pixelColor = palette.getRGB(pixel);
+                        color.red += pixelColor.red;
+                        color.green += pixelColor.green;
+                        color.blue += pixelColor.blue;
+                    }
+                }
+                double area = (double) rect.width * rect.height;
+                color.red = (int) Math.min(255, ((double) color.red) / area);
+                color.green = (int) Math.min(255, ((double) color.green) / area);
+                color.blue = (int) Math.min(255, ((double) color.blue) / area);
+
+                for (int i = 0; i < rect.width; i++) {
+                    x = (rect.x - startPosX) + i;
+                    for (int j = 0; j < rect.height; j++) {
+                        y = (rect.y - startPosY) + j;
+                        imageData.setPixel(x,y, palette.getPixel(color));
                     }
                 }
             }
-            iterateOverOuterX(width, height, imageData, lineWidth, lineWidth, boxSizeX, boxSizeY, colors, size, palette);
+
             Image newImage = new Image(this.graphicsContext.getDevice(), imageData);
             this.graphicsContext.drawImage(newImage, intersectRect.x, intersectRect.y);
             newImage.dispose();
         }
         return ret;
-    }
-
-    private void iterateOverOuterX(int width, int height, ImageData imageData, int boxWidth, int boxHeight, int boxSizeX, int boxSizeY, RGB[][] colors, int[][] size, PaletteData palette) {
-        int i = 0;
-        while (true) {
-            iterateOverOuterY(width, height, imageData, boxWidth, boxHeight, boxSizeY, colors[i], size[i], palette, i);
-
-            if (++i >= boxSizeX) {
-                return;
-            }
-        }
-    }
-
-    private void iterateOverOuterY(int width, int height, ImageData imageData, int boxWidth, int boxHeight, int boxSizeY, RGB[] color, int[] ints, PaletteData palette, int i) {
-        int j = 0;
-        while (true) {
-            if (color[j] != null) {
-                color[j].red = (int) Math.min(255, ((double) color[j].red) / ((double) ints[j]));
-                color[j].green = (int) Math.min(255, ((double) color[j].green) / ((double) ints[j]));
-                color[j].blue = (int) Math.min(255, ((double) color[j].blue) / ((double) ints[j]));
-
-                iterateOverInnerX(width, height, imageData, boxWidth, boxHeight, palette.getPixel(color[j]), i, j);
-            }
-            if (++j >= boxSizeY) {
-                return;
-            }
-        }
-    }
-
-    private void iterateOverInnerX(int width, int height, ImageData imageData, int boxWidth, int boxHeight, int pixel, int i, int j) {
-        int i2 = 0;
-        while (true) {
-            iterateOverInnerY(width, height, imageData, boxWidth, boxHeight, pixel, i, j, i2);
-            if (++i2 >= boxWidth) {
-                return;
-            }
-        }
-    }
-
-    private void iterateOverInnerY(int width, int height, ImageData imageData, int boxWidth, int boxHeight, int pixel, int i, int j, int i2) {
-        int j2 = 0;
-        while (true) {
-            calculatePixel(width, height, imageData, boxWidth, boxHeight, pixel, i, j, i2, j2);
-            if (++j2 >= boxHeight) {
-                return;
-            }
-        }
-    }
-
-    private void calculatePixel(int width, int height, ImageData imageData, int boxWidth, int boxHeight, int pixel, int i, int j, int i2, int j2) {
-        int x;
-        int y;
-        x = i * boxWidth + i2;
-        y = j * boxHeight + j2;
-        if (x >= width || y >= height) {
-            return;
-        }
-        if (x < 0 || y < 0) {
-            return;
-        }
-
-        imageData.setPixel(x, y, pixel);
     }
 }
